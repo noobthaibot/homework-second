6ec93c570f28a84919f40b29115813e2ed6ddff3
noobthaibot <88024655+noobthaibot@users.noreply.github.com>
2021-10-10 15:39:44 +0300

    test

--- js/scrips.js
+++ js/scrips.js
@@ -1,3 +1,10 @@
 $(document).ready(function(){
     $('.slider').slick();
 });  
+$(document).ready(function(){
+    $('.slider-menu').slick({
+        arrows:true,
+        slidesToShow: 4, 
+        slidesToScroll: 1
+    });
+});  
